f2680d7e9e6c03e980a721b369e0944e2a409c06
JoseFrancisc0 <jose.wong@utec.edu.pe>
2025-05-20 01:13:21 -0500

    Init RTree index class

---
+++ RTree/rtree_index.py
@@ -0,0 +1,45 @@
+from rtree import index
+
+class RTreeIndex:
+    """
+    Manejo de archivos fisicos usando un índice bajo una estructura RTree.
+    La metadata maneja {key : (lat, long)} para cada registro del archivo
+    """
+    def __init__(self, index_name="rtree_index", main_file="main_records.dat", record_size=None):        
+        self.main_file = main_file
+        self.record_size = record_size
+    
+        p = index.Property()
+        p.dimension = 2
+        p.overwrite = False
+
+        self.rtree_idx = index.Index(index_name, properties=p)
+
+    def add(self, data):
+        """
+        Insertamos un record al árbol .Asumimos que la data
+        viene enpaquetada antes de insertar al RTree.
+        La librería maneja puntos como rectangulos de area 0.
+        """
+        pass
+
+    def search(self, lat, lon):
+        """
+        TODO: interpretar search() para RTree.
+        Por mientras, aplicar KNN() sobre un rectangulo pequeño (k=1)
+        """
+        pass
+
+    def range_search(self, begin_key, end_key):
+        """
+        Buscar puntos dentro de un box definido por (begin_key) y (end_key).
+        Devuelve unicamente los indices que debe leer la clase principal
+        """
+        pass
+
+    def erase(self, key):
+        """
+        Eliminacion de un record en específico.
+        Falta ver como lo hace xd
+        """
+        pass
