cbde15a319b106d505eade43a291d0d64caadc1d
JoseFrancisc0 <jose.wong@utec.edu.pe>
2025-05-25 14:47:57 -0500

    reworked record file for rtree demo

--- RTree/record.py
+++ RTree/record.py
@@ -1,153 +1,144 @@
 import struct
 import os
-from rtree_index import RTreeIndex
 
-class Record:
-    FORMAT = "i20sdd"
-    SIZE = struct.calcsize(FORMAT)
+REGISTER_FORMAT = 'i 60s i 5s 55s i 3s 35s f f 10s' 
+RECORD_SIZE = struct.calcsize(REGISTER_FORMAT)
 
-    def __init__(self, id, name, lat, lon):
-        self.id = id
-        self.name = name
-        self.lat = lat
-        self.lon = lon
-    
-    def display(self):
-        return f"[{self.id}] {self.name} : ( {self.lat} , {self.lon} )"
+class Registro: # Cities
+    def __init__(self, id=0, name="", state_id=0, state_code="", state_name="",
+                 country_id=0, country_code="", country_name="", latitude=0.0,
+                 longitude=0.0, wikiDataId=""): #para constructor "vacio"
+        
+        self.id = id                                        
+        self.name = name                   
+        self.state_id = state_id            
+        self.state_code = state_code        
+        self.state_name = state_name        
+        self.country_id = country_id        
+        self.country_code = country_code        
+        self.country_name = country_name        
+        self.latitude = latitude        
+        self.longitude = longitude      
+        self.wikiDataId = wikiDataId        
+
+    def get_reg_string(self): # vars(self).values() retorna el valor de todas las variables de la clase
+        return " | ".join(str(v) for v in vars(self).values()) # name | state_id | state_code | ...
+        
+    def to_fields(self): #Encodifica todos los campos, asi puedo usar * 
+        return (
+        self.id,
+        self.name.encode(),
+        self.state_id,
+        self.state_code.encode(),
+        self.state_name.encode(),
+        self.country_id,
+        self.country_code.encode(),
+        self.country_name.encode(),
+        self.latitude,
+        self.longitude,
+        self.wikiDataId.encode()
+    )
 
     def pack(self):
         return struct.pack(
-            Record.FORMAT,
-            self.id,
-            self.name.encode(),
-            self.lat,
-            self.lon
+            REGISTER_FORMAT,
+            *self.to_fields()
         )
 
-    @staticmethod
-    def unpack(data):
-        unpacked = struct.unpack(Record.FORMAT, data)
-        return Record(
-            unpacked[0],
-            unpacked[1].decode().rstrip('\x00').strip(),
-            unpacked[2],
-            unpacked[3]
+    @classmethod  #truquito que vi para poder leer todo de una vez a una clase -> reg = Registro.from_bytes(data)
+    def from_bytes(cls, byte_data):
+        unpacked = struct.unpack(REGISTER_FORMAT, byte_data)
+        return cls(
+            unpacked[0],  # id
+            unpacked[1].decode('utf-8', errors='replace').strip(),  # name
+            unpacked[2],  # state_id
+            unpacked[3].decode().strip(),  # state_code
+            unpacked[4].decode().strip(),  # state_name
+            unpacked[5],  # country_id
+            unpacked[6].decode().strip(),  # country_code
+            unpacked[7].decode('utf-8', errors='replace').strip(),  # country_name
+            unpacked[8],  # latitude
+            unpacked[9],  # longitude
+            unpacked[10].decode('utf-8', errors='replace').strip()  # wikiDataId
         )
 
-class RecordFile:
-    HEADER_SIZE = 4
+    def print_reg(self):
+        print(self.get_reg_string())
+
+class FreeList:
+    FORMAT = 'i'
+    SIZE = struct.calcsize(FORMAT)
+
     def __init__(self, filename):
-        """
-        Archivo almacén de records. Será la 
-        base del índice creado con el RTree.
-        Utiliza un header para la free list.
-        """
         self.filename = filename
-        self.index = RTreeIndex(index_name=f"{filename}_index", main_file=filename, record_size=Record.SIZE)
+        if not os.path.exists(self.filename):
+            with open(self.filename, 'wb'):
+                pass
+    
+    def get_all(self):
+        with open(self.filename, 'rb') as f:
+            data = f.read()
+            return [pos[0] for pos in struct.iter_unpack(self.FORMAT, data)]
+    
+    def add(self, pos):
+        with open(self.filename, 'ab') as f:
+            f.write(struct.pack(self.FORMAT, pos))
+    
+    def pop(self):
+        positions = self.get_all()
+        if not positions: 
+            return None
+        pos = positions.pop(0)
+        
+        with open(self.filename, 'wb') as f:
+            for p in positions:
+                f.write(struct.pack(self.FORMAT, p))
+            
+        return pos
 
-        if not os.path.exists(self.filename) or os.path.getsize(self.filename) < self.HEADER_SIZE:
+class FixedRecord:
+    def __init__(self, filename):
+        self.filename = filename
+        self.metadata = FreeList(filename + ".free")
+
+        if not os.path.exists(self.filename):
             with open(self.filename, 'wb') as file:
-                file.write(struct.pack('i'), -1)
-    
-    def _read_header(self):
-        """
-        Lee el header del archivo.
-        """
+                pass
+
+    def load(self):
+        registros = []
         with open(self.filename, 'rb') as file:
-            return struct.unpack('i', file.read(self.HEADER_SIZE))[0]
-    
-    def _write_header(self, value):
-        """
-        Sobreescribe el header del archivo.
-        Util para actualizar la free list.
-        """
+            pos = 0
+            while True:
+                data = file.read(RECORD_SIZE)
+                if not data or len(data) < RECORD_SIZE:
+                    break
+                registros.append((pos, Registro.from_bytes(data)))
+                pos += 1
+
+        free_positions = set(self.metadata.get_all())
+        return [registro for i, registro in registros if i not in free_positions]
+
+    def add(self, registro):
+        pos = self.metadata.pop()
         with open(self.filename, 'r+b') as file:
-            file.write(struct.pack('i', value))
-    
-    def add(self, record):
-        """
-        Inserta un nuevo record al archivo.
-        No interesa preservar el orden aquí.
-        """
-        free_pos = self._read_header()
-        with open(self.filename, 'r+b') as file:
-            if free_pos == -1:
-                file.seek(0, os.SEEK_END)
-                file.write(record.pack())
+            if pos is not None:
+                file.seek(pos * RECORD_SIZE)
             else:
-                file.seek(self.HEADER_SIZE + (free_pos * Record.SIZE) + (Record.SIZE - 4) )
-                next_free = file.read(4)
-                next_free = struct.unpack('i', next_free)[0]
-                file.seek(self.HEADER_SIZE + free_pos * Record.SIZE)
-                file.write(record.pack())
-                self._write_header(next_free)
-        
-        # Confirmado el insert, pasamos al RTree
-        self.index.add(record)
+                file.seek(0, os.SEEK_END)
+            file.write(registro.pack())
 
-    def search(self, lon, lat):
-        """
-        Búsqueda dentro del archivo. Se
-        apoya en el método del RTree.
-        """
-        coords = (lon, lat)
-        match_idx = self.index.search(coords)[0]
-        
-        if not match_idx:
-            return None
-
+    def read(self, pos):
         with open(self.filename, 'rb') as file:
-            file.seek(self.HEADER_SIZE + match_idx * Record.SIZE)
-            record_bytes = file.read(Record.SIZE)
-            if len(record_bytes) == Record.SIZE:
-                record = Record.unpack(record_bytes)
-                return record
+            file.seek(pos * RECORD_SIZE)
+            data = file.read(RECORD_SIZE)
+            if not data or len(data) < RECORD_SIZE:
+                return None
+            return Registro.from_bytes(data)
 
-    def range_search(self, min_lon, min_lat, max_lon, max_lat):
-        """
-        Búsqueda de rango dentro del archivo.
-        Se apoya en el método del RTree.
-        """
-        min_coords = (min_lon, min_lat)
-        max_coords = (max_lon, max_lat)
-        match_ids = self.index.range_search(min_coords, max_coords)
-        
-        if not match_ids:
-            return None
+    def print_all(self):
+        for reg in self.load():
+            reg.print_reg()
 
-        found_records = []
-
-        with open(self.filename, 'rb') as file:
-            for idx in match_ids:
-                file.seek(self.HEADER_SIZE + idx * Record.SIZE)
-                record_bytes = file.read(Record.SIZE)
-                if len(record_bytes) == Record.SIZE:
-                    record = Record.unpack(record_bytes)
-                    found_records.append(record)
-        
-        return found_records
-
-    def remove(self, key):
-        """
-        Borra elementos usando el método dentro del RTree.
-        """
-
-        # Buscamos las coordenadas del record
-        coords = (None, None)
-        with open(self.filename, 'rb') as file:
-            file.seek(self.HEADER_SIZE + key * Record.SIZE)
-            record_bytes = file.read(Record.SIZE)
-            if len(record_bytes) == Record.SIZE:
-                record = Record.unpack(record_bytes)
-                coords = (record.lon, record.lat)
-        
-        # Actualizamos la free list
-        free_pos = self._read_header()
-        with open(self.filename, 'r+b') as file:
-            deleted = Record(0, "", 0.0, 0.0, free_pos)
-            file.seek(self.HEADER_SIZE + key * Record.SIZE)
-            file.write(deleted.pack())
-        self._write_header(key)
-
-        # Eliminamos en el arbol
-        self.index.erase(key, coords)
+    def remove(self, pos):
+        self.metadata.add(pos)
